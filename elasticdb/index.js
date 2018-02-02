304297c6bdc24042c5d5a5d9bd015c93945b2bf0
Jarrod Powell <jpowell1032@gmail.com>
2018-02-02 15:12:43 -0800

    Optimize elastic search querying

--- elasticdb/index.js
+++ elasticdb/index.js
@@ -1,24 +1,8 @@
 var elasticsearch = require('elasticsearch');
-var Bluebird = require('bluebird');
 const ELASTIC_PORT = process.env.ELASTIC_PORT
 var client = new elasticsearch.Client({
   host: ELASTIC_PORT,
-  log: 'trace',
-  defer: function () {
-    // for older Bluebird
-     return Bluebird.defer();
-
-    // var resolve, reject;
-    // var promise = new PromiseImpl(function() {
-    //   resolve = arguments[0];
-    //   reject = arguments[1];
-    // });
-    // return {
-    //   resolve: resolve,
-    //   reject: reject,
-    //   promise: promise
-    // };
-  }
+  log: 'trace'
 });
 
 
@@ -32,38 +16,39 @@ client.ping({
   }
 });
 
-// client.create({
-//   index: 'searchResults',
-//   type: 'listings',
-//   body: {
-//     title: 'Comfy Cambridge Home'
-//   }
-// })
-
 function searchKeyword (keyword) {
+  console.log('searching elastic:', keyword)
   return client.search({
     index: 'airbnb',
     type: 'listings',
     body: {
       query: {
-        fuzzy: {
-          "title": {
-            value: keyword,
-
-          }
+        bool: {
+          "must": [
+                  {
+                    "query_string": {
+                      "default_operator": "OR",
+                      "query": keyword,
+                      // "cutoff_frequency": 0.001
+                      // "analyze_wildcard": true
+                    }
+                  },
+                  {
+                    "exists": {
+                      "field": "name"
+                    }
+                  }
+                ],
+          // "filter":  {
+          //   "stop": {
+          //     "type": "stop",
+          //     "stopwords": ["near", "and", "the", "in", "close", "to"]
+          //   }
+          // }    
         }
       }
     }
   })
-  //   .then( (body) => {
-  //   let hits = body.hits.hits;
-  //   console.log('hits: ', hits)
-  // }, (error) => {
-  //   console.log('error!', error.message);
-  // });
-  }
-
-Bluebird.resolve()
-  .then(searchKeyword)  
+} 
 
 module.exports.searchKeyword = searchKeyword
